584bf6df74f6c8f88702f5468a34a7e93b310bc9
Abdul Rahman <rahmansiddiqui331@gmail.com>
2024-11-01 07:03:33 -0700

    first commit

---
+++ src/app/[id]/page.tsx
@@ -0,0 +1,16 @@
+// import React from 'react'
+
+// const Id = async({params, searchParams}:any) =>  {
+//     const {id} = await params;
+//     const {search, age} = await searchParams;
+//     console.log(search, age);
+//   return (
+//     <div>
+//         <h2>Path parameter {id}</h2>
+//         <h2>Query Parameter {search}</h2>
+//         <h2>Query Parameter {age}</h2>
+//     </div>
+//   )
+// }
+
+// export default Id
